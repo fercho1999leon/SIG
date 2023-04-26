d445ff9d246b69e18e03752873be2f9a55503d40
fercho1999leon <fercho1999_w@hotmail.com>
2023-04-26 13:40:33 -0500

    CORREPCION EN LOGO REPORTE

--- resources/views/partials/encabezados/reporte-institucional.blade.php
+++ resources/views/partials/encabezados/reporte-institucional.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row text-center m-3">
-        <img class="img-fluid w-50" src="{{ secure_asset('img/logo/logoSolicitud.png') }}" alt="" style="opacity: 0.7;">
+        <img class="img-fluid w-50" width="300px" src="{{ secure_asset('img/logo/logoSolicitud.png') }}" alt="" style="opacity: 0.7;">
     </div>
     <div class="row text-center m-3">
         <div class="col-12 m-3">
