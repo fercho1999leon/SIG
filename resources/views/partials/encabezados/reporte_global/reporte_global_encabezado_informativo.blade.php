f5f4d41dabbf2b9bfad84badf59cd38b8655126a
fercho1999leon <fercho1999_w@hotmail.com>
2023-07-18 14:28:33 -0500

    BUGS EN ACTA

--- resources/views/partials/encabezados/reporte_global/reporte_global_encabezado_informativo.blade.php
+++ resources/views/partials/encabezados/reporte_global/reporte_global_encabezado_informativo.blade.php
@@ -1,20 +1,23 @@
+<style>
+    .interlineado{
+        line-height:normal;
+    }
+</style>
 <div class="row mb-3">
     <div class="col-xs-6 text-left">
-        <p class="textoInformativo text-uppercase"><strong>CARRERA:</strong> {{ $carrera->nombre }}</p>
-        <p class="textoInformativo text-uppercase"><strong>MATERIA:</strong> {{ $materia->nombre }}</p>
+        <p class="textoInformativo text-uppercase interlineado"><strong>CARRERA:</strong> {{ $carrera->nombre }}</p>
+        <p class="textoInformativo text-uppercase "><strong>MATERIA:</strong> {{ $materia->nombre }}</p>
         @if ($docente == null) 
-        <p class="textoInformativo text-uppercase"><strong>DOCENTE:</strong> S/N </p>    
+        <p class="textoInformativo text-uppercase "><strong>DOCENTE:</strong> S/N </p>    
         @endif
         @if ($docente != null || $docente != "")
-        <p class="textoInformativo text-uppercase"><strong>DOCENTE:</strong> {{ $docente->nombres }}
+        <p class="textoInformativo text-uppercase "><strong>DOCENTE:</strong> {{ $docente->nombres }}
             {{ $docente->apellidos }}</p>    
         @endif
-        
-
     </div>
     <div class="col-xs-6 text-left">
-        <p class="textoInformativo text-uppercase"><strong>FECHA:</strong> {{ $fecha }}</p>
-        <p class="textoInformativo text-uppercase"><strong>SEMESTRE:</strong> {{ $semestre->nombsemt }}</p>
-        <p class="textoInformativo text-uppercase"><strong>PARALELO:</strong> {{ $curso->paralelo }}</p>
+        <p class="textoInformativo text-uppercase "><strong>FECHA:</strong> {{ $fecha }}</p>
+        <p class="textoInformativo text-uppercase "><strong>SEMESTRE:</strong> {{ $semestre->nombsemt }}</p>
+        <p class="textoInformativo text-uppercase "><strong>PARALELO:</strong> {{ $curso->paralelo }}</p>
     </div>
 </div>
